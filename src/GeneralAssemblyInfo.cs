48b17463f2a02094b5b59e56d392400da0e235aa
Andre Guilherme <andregsilv@gmail.com>
2016-02-03 17:40:43 -0200

    General AssemblyInfo

---
+++ src/GeneralAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Mniak")]
+[assembly: AssemblyProduct("MniakLibs")]
+[assembly: AssemblyCopyright("Copyleft © Andre Soares 2014-Forever")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("527663f8-b987-48eb-bc32-89cf3012c915")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
