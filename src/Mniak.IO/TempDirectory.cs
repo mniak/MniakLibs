5fc85cfa630e56b472887bdedf3928ca9cd03011
Andre Guilherme <andregsilv@gmail.com>
2016-02-04 09:48:02 -0200

    Add TempDirectory

---
+++ src/Mniak.IO/TempDirectory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mniak.IO
+{
+    public sealed class TempDirectory : IDisposable
+    {
+        private bool disposed = false;
+        public TempDirectory(string path)
+        {
+            this._path = path;
+            if (!Directory.Exists(Path))
+                Directory.CreateDirectory(Path);
+        }
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+
+            Directory.Delete(Path, true);
+        }
+        private string _path;
+        public string Path
+        {
+            get
+            {
+                if (disposed)
+                    throw new ObjectDisposedException("TemporaryDirectory");
+
+                return _path;
+            }
+        }
+    }
+}
