d4f4f2584db5db568d74d5b0d07a7180e948a8b6
Andre Guilherme <andregsilv@gmail.com>
2016-10-19 10:42:41 -0200

    Add ReversibleAttributesPath

---
+++ src/Mniak.IO/ReversibleAttributesPath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateReferences
+{
+    internal class ReversibleAttributes : IDisposable
+    {
+        FileAttributes attributes;
+        private readonly string path;
+
+        public ReversibleAttributes(string path)
+        {
+            this.path = path;
+            this.attributes = File.GetAttributes(this.path);
+            File.SetAttributes(this.path, this.attributes & ~FileAttributes.ReadOnly);
+        }
+
+        public void Dispose()
+        {
+            File.SetAttributes(this.path, this.attributes);
+        }
+    }
+}
